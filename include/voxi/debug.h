33334638fc631491dd2f98a762f4ae002bb49c97
erl <erl@9654bea8-792f-0410-a6e5-c0d18bf2b27d>
2002-10-23 08:44:14 +0000

    Re-organized the tests for _POSIX_THREADS, NDEBUG, HAVE_CPP_VARARGS.

--- include/voxi/debug.h
+++ include/voxi/debug.h
@@ -40,8 +40,7 @@
 
 
 #ifndef DEBUG_H
-#define DEBUG_H
-
+#  define DEBUG_H
 
 /**
   @var static int debug  
@@ -55,32 +54,47 @@
   It seems to be because of the 'static'-modifier. Fix this somehow.. 
 */
 
-#ifndef DEBUG_MESSAGES
-#define DEBUG_MESSAGES 0
-#endif
+#  ifndef DEBUG_MESSAGES
+#    define DEBUG_MESSAGES 0
+#  endif
 
-#ifndef NDEBUG
+#  ifndef NDEBUG
 static int debug = DEBUG_MESSAGES;
-#define BUILD_INFO_STR "debug build"
-#else
-#define BUILD_INFO_STR "release build"
-#endif /* NDEBUG */
+#    define BUILD_INFO_STR "debug build"
+#  else
+#    define BUILD_INFO_STR "release build"
+#  endif /* NDEBUG */
 
-#ifndef WIN32
-
-/* For any ISO C99 compliant compiler the following macros are correct. */
-
-#ifndef NDEBUG
-#include <stdio.h>
-
-#ifdef _POSIX_THREADS
-#include <pthread.h>
-
-/** @def DEBUG(x...) printf-like output that can be turned of via the static variable debug
-
-    but with info about file, calling function, thread-id and line-number
+/*
+  This file is very messy due to the many configuration variants that need to 
+  be handled. Basically, there are three preprocessor variables that influence
+  the definitions in this file - NDEBUG, HAVE_CPP_VARARGS, and _POSIX_THREADS
+  
+  Also note that all ISO C99 compilers should HAVE_CPP_VARARGS, but MSVC++ 
+  is apparently not ISO C99 compatible.
+  
+  First, define the DEBUG macro/function
 */
-#define DEBUG(x...) \
+
+#  if defined(NDEBUG) 
+#    if HAVE_CPP_VARARGS
+/* non-debug build with a c preprocessor supporting variable argument lists */
+#      define DEBUG(x...)
+#    else
+/* non-debug build with a c preprocessor *not* supporting variable argument 
+   lists */
+__inline void DEBUG(char *str, ...) {}
+#    endif
+#  else /* a debug build */
+#    include <stdio.h> /* all debug configurations needs stdio */
+/** @def DEBUG(x...) printf-like output that can be turned of via the static 
+    variable debug but with info about file, calling function, thread-id and 
+    line-number
+*/
+#    if HAVE_CPP_VARARGS
+#      if defined(_POSIX_THREADS)
+/* debug build with C preprocessor varargs and Posix threads */
+#        define DEBUG(x...) \
  do if(debug!=0){\
      fprintf(stderr, "DEBUG: %s, %s[%ld]:%d: ", \
              __FILE__, \
@@ -89,8 +103,8 @@ static int debug = DEBUG_MESSAGES;
              __LINE__);\
      fprintf(stderr, ## x);\
  } while(0)
-#else /* if POSIX_THREADS ... else */
-#define DEBUG(x...) \
+#      else /* defined(_POSIX_THREADS) ... else ... */
+#        define DEBUG(x...) \
  do if(debug!=0){\
      fprintf(stderr, "DEBUG: %s, %s:%d: ", \
              __FILE__, \
@@ -98,39 +112,11 @@ static int debug = DEBUG_MESSAGES;
              __LINE__);\
      fprintf(stderr, ## x);\
  } while(0)
-#endif /* if POSIX_THREADS ... else ... */
-#else  /* Debugging disabled */
-#define DEBUG(x...)
-#endif
+#      endif
+#    else 
+/* C preprocess without varargs. Apparently don't care about threads here  */
+#      include <stdarg.h>
 
-/** @def DIAG(x...) printf-like output
-
-    but with info about file, calling function, thread-id and line-number
-*/
-#define DIAG(x...) \
- do {\
-     fprintf(stderr, "%s, %s[%ld]:%d: ", \
-             __FILE__, \
-             __PRETTY_FUNCTION__, \
-             (long)pthread_self(),\
-             __LINE__);\
-     fprintf(stderr, ## x);\
- } while(0)
-
-
-#else  /* WIN32 */
-
-/**
-   Kludge since MSVC does allow variable number of args and hence does
-   not follow the ISO C99 standard. Instead we use inline-functions. See note
-   in the head.
-*/
-#ifndef NDEBUG
-#include <stdio.h>
-#include <stdarg.h>
-#endif /* NDEBUG */
-
-#ifndef NDEBUG
 static void DEBUG(char *str, ...)
 {
   if(debug) {
@@ -141,7 +127,40 @@ static void DEBUG(char *str, ...)
     va_end(args);
   } 
 }
+#    endif /* if cpp_varargs ... else ... */
+#  endif /* if a debug build ... else ... */
 
+/*
+  Now define the DIAG function which is defined regardless of NDEBUG state.
+  However it depends on _POSIX_THREADS and CPP_VARARGS 
+*/
+#  if HAVE_CPP_VARARGS
+#    if defined(_POSIX_THREADS)
+/** @def DIAG(x...) printf-like output
+
+    but with info about file, calling function, thread-id and line-number
+*/
+#      define DIAG(x...) \
+ do {\
+     fprintf(stderr, "%s, %s[%ld]:%d: ", \
+             __FILE__, \
+             __PRETTY_FUNCTION__, \
+             (long)pthread_self(),\
+             __LINE__);\
+     fprintf(stderr, ## x);\
+ } while(0)
+#    else /* cpp_varargs but not posix_threads */
+#      define DIAG(x...) \
+ do {\
+     fprintf(stderr, "%s, %s:%d: ", \
+             __FILE__, \
+             __PRETTY_FUNCTION__, \
+             __LINE__);\
+     fprintf(stderr, ## x);\
+ } while(0)
+#    endif /* if posix_threads */
+#  else /* if cpp_varargs ... else ... */
+/* regardless of _POSIX_THREADS or not */
 static void DIAG(char *str, ...)
 {
     va_list args;
@@ -150,12 +169,6 @@ static void DIAG(char *str, ...)
     vfprintf(stderr, str, args);
     va_end(args);
 }
-
-#else  /* Debugging disabled */
-__inline void DEBUG(char *str, ...) {}
-__inline void DIAG(char *str, ...) {}
-#endif /* NDEBUG */
-
-#endif /* WIN32 */
+#  endif
 
 #endif /* DEBUG_H */
