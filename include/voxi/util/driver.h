51c25b36a67027544b19201b1b2a7700471d5e9f
erl <erl@9654bea8-792f-0410-a6e5-c0d18bf2b27d>
2003-09-08 12:07:09 +0000

    New code to handle drivers implemented as shared libraries / DLLs.

---
+++ include/voxi/util/driver.h
@@ -0,0 +1,38 @@
+/*
+ * driver.h
+ *
+ * Helper code for drivers - that is code in a shared library/DLL loaded at 
+ * runtime, containing functions with known names, which are stored as
+ * function pointers in a driver struct
+ */
+
+#include <stddef.h> /* size_t defined here */
+#include <voxi/util/err.h>
+#include <voxi/util/shlib.h>
+
+/*
+ *  Type definitions
+ */
+typedef struct sDriverFunctionInfo
+{
+  const char *name;
+  size_t offset;
+} sDriverFunctionInfo, *DriverFunctionInfo;
+
+typedef Error (*DriverGetAPIVersionFuncPtr)( int *version );
+
+/*
+  Error codes
+  */
+enum { ERR_DRIVER_UNSPECIFIED };
+
+/*
+  driverOpen assumes that the driver has a function called 'getAPIVersion' 
+  which returns the API version for which the driver was written 
+  */
+EXTERN_UTIL Error driverOpen( const char *name, int *apiVersion, 
+                              void *driverStruct );
+
+EXTERN_UTIL Error driverLoadFunctions( SharedLibrary shlib, int functionCount,
+                                       sDriverFunctionInfo *driverFunctions, 
+                                       void *driverStruct );
