d1304e2807e395489871011c35afe2a21f11df54
mst <mst@9654bea8-792f-0410-a6e5-c0d18bf2b27d>
2004-03-11 15:29:26 +0000

    LOG_LEVEL convenience macro

--- include/voxi/util/logging.h
+++ include/voxi/util/logging.h
@@ -104,8 +104,11 @@ EXTERN_UTIL Error log_noLogError( Logger logger, const char *moduleName,
  *   ErrDispose(error, ...);
  *
  */
+
 #define LOG_LEVEL_SET(Level) \
   _voxiUtilLogLevel = (Level);
+  
+#define LOG_LEVEL _voxiUtilLogLevel
   
 #define LOG_MODULE_DECL(moduleName, defaultLevel) \
   static LogLevel _voxiUtilLogLevel = (defaultLevel); \
